fc0f4fedd7794e401e85b0b323755a1f07b3c6f1
Thomas <muethb20@htl-kaindorf.at>
2024-10-03 12:19:54 +0200

    [FEAT] shadcn table for tasks implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Header from "@/components/header/Header.tsx";
+import {TodosPage} from "@/components/pages/todos/Todos.page.tsx";
+
 
 function App() {
 
   return (
-          <Routes>
-              <Route path="/" component={<Header/>} />
-          </Routes>
+       <Routes>
+            <Route path={'/'} element={<Header/>} ></Route>
+            <Route path={'/todos'} element={<TodosPage/>} ></Route>
+       </Routes>
   )
 }
 
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
-import {Outlet} from "react-router-dom";
 
 
 const Header: React.FC = () => {
     return (
         <div>
-            <h1>Teso</h1>
-            <Outlet></Outlet>
+            <h1>Test</h1>
         </div>
     );
 };
